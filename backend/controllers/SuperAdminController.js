19b6fe23072d92c611a3cf27e9ae7d2e117f59c5
Revolt-47 <i201806@nu.edu.pk>
2023-11-18 22:03:08 +0500

    Added forgot password to super admin in backend

--- backend/controllers/SuperAdminController.js
+++ backend/controllers/SuperAdminController.js
@@ -28,6 +28,76 @@ async function loginSuperAdmin(req, res) {
   }
 }
 
+async function forgotPassword(req, res) {
+  const { email } = req.body;
+
+  // Find the admin by email
+  const admin = await SuperAdmin.findOne({ email });
+
+  if (!admin) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  // Generate a unique reset token
+  const resetToken = crypto.randomBytes(16).toString('hex'); // You can adjust the token length
+
+  // Calculate the expiration time for the reset link (10 minutes from now)
+  const expirationTime = Date.now() + 600000; // 10 minutes in milliseconds
+
+  // Include the reset token and expiration time in the reset link
+  const resetLink = `http://localhost:3000/reset-password/${admin._id}/${resetToken}/${expirationTime}`;
+  
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'amirdaniyal47@gmail.com',
+      pass: 'zucq jdwo iqwp wttd',
+    },
+  });
+
+  const mailOptions = {
+    from: 'amirdaniyal47@gmail.com',
+    to: admin.email,
+    subject: 'Password Reset Link',
+    text: `Click on the following link to reset your password: ${resetLink}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'An error occurred while sending the reset link.' });
+    }
+
+    res.status(200).json({ message: 'Password reset link sent to your email' });
+  });
+}
+
+async function resetPassword(req, res) {
+  const { adminId, resetToken, expirationTime, newPassword } = req.body;
+  
+  // Find the admin by ID
+  const admin = await admin.findById(adminId);
+
+  if (!admin) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  // Check if the expiration time has passed (link is expired)
+  if (expirationTime < Date.now()) {
+    return res.status(401).json({ error: 'Reset link has expired' });
+  }
+
+
+  admin.password = newPassword;
+
+  await admin.save();
+
+  res.status(200).json({ message: 'Password reset successful' });
+}
+
+
 module.exports = {
   loginSuperAdmin,
+  forgotPassword,
+  resetPassword
 };
