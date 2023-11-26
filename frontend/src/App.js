89eed0d4350d07b49d6ee1621dc6b5c9dd084f34
Revolt-47 <i201806@nu.edu.pk>
2023-11-26 14:43:57 +0500

    restored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import SuperAdminHomePage from './SuperAdminScreens/HomePage';
 import SuperAdminLogin from './SuperAdminScreens/Login';
 import SchoolDetailsScreen from './SuperAdminScreens/SchoolDetails';
 import { BrowserRouter as Router } from 'react-router-dom';
-import PaymentForm from './PaymentScreens/InititalPayment';
+// import PaymentForm from './PaymentScreens/InititalPayment';
 
 const isAuthenticated = () => {
   // Check if the token is present and valid
@@ -31,8 +31,8 @@ const App = () => {
           <Route path="/superadmin/home" element={<AuthRoute element={<SuperAdminHomePage />} />} />
           <Route path="/superadmin/allschools" element={<AuthRoute element={<AllSchoolsScreen />} />} />
           <Route path="/superadmin/schooldetails/:id" element={<AuthRoute element={<SchoolDetailsScreen />} />} />
-          <Route path="/login" element={<SuperAdminLogin />}  />
-          <Route path="/:schoolId" element={<PaymentForm />}/>
+          <Route path="/" element={<SuperAdminLogin />}  />
+          
          
         </Routes>
       </Router>
