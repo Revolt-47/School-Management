49edc278dcecf0fee57ef995fe850460fbe70893
Revolt-47 <i201806@nu.edu.pk>
2024-02-15 10:12:14 +0500

    payment microservice made

---
+++ payment-managment/routes/PaymentRouter.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const paymentController = require('../controllers/PaymentController');
+const router = express.Router();
+const {VerifyRegistrationToken,VerifySchoolorAdmin} = require('../utils/Authenticate')
+// Endpoint to create an initial payment
+router.post('/initial-payment', paymentController.createInitialPayment);
+
+// Endpoint to handle successful initial payment
+router.post('/handle-successful-initial-payment', paymentController.handleSuccessfulInitialPayment);
+
+// Endpoint to create a monthly subscription
+router.post('/monthly-subscription', async (req, res) => {
+  try {
+    const { schoolId} = req.body;
+    // Create a monthly subscription
+    const subscription = await paymentController.createMonthlySubscription(schoolId);
+    res.status(200).json({ message: 'Monthly subscription created successfully.', subscription });
+  } catch (error) {
+    console.error('Error creating monthly subscription:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.post('/pause-subscription',VerifyRegistrationToken,VerifySchoolorAdmin,paymentController.pauseSubscription);
+router.post('/resume-subscription',VerifyRegistrationToken,VerifySchoolorAdmin,paymentController.resumeSubscription);
+
+
+// Add more endpoints for other payment functions...
+
+module.exports = router;
