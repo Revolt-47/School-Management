9f95665979c6abb3ed07826117aa9305385ac5d5
Muhammad Abdullah <writetomuhammadabdullah@gmail.com>
2024-02-18 13:47:30 +0500

    Updated the styling of frontpage

--- schl_mgmt_frontend/src/components/FrontPage/FrontPage.jsx
+++ schl_mgmt_frontend/src/components/FrontPage/FrontPage.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 import Cookies from 'js-cookie';
+import { BiCar, BiChevronDown, BiBuilding, BiSolidWatch } from 'react-icons/bi'; // Importing icons
 import StudentCountBox from './StudentCountBox';
+import logo from './van guardian logo.png'; // Importing logo
 
 function FrontPage() {
   const [schoolData, setSchoolData] = useState(null);
@@ -10,6 +12,7 @@ function FrontPage() {
   const [closeTime, setCloseTime] = useState('');
   const [showUpdateForm, setShowUpdateForm] = useState(false);
   const [totalStudents, setTotalStudents] = useState(0);
+  const [totalDrivers, setTotalDrivers] = useState(0); // New state for total drivers
 
   const fetchData = async () => {
     try {
@@ -35,6 +38,18 @@ function FrontPage() {
       });
       const studentsData = await studentsResponse.json();
       setTotalStudents(studentsData.totalStudentsCount);
+
+      // Fetch total number of drivers
+      const driversResponse = await fetch(`http://localhost:3000/driver/getAllDriversofSchool`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({schoolId:id})
+      });
+      const driversData = await driversResponse.json();
+      setTotalDrivers(driversData.length);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -80,72 +95,130 @@ function FrontPage() {
   };
 
   return (
-    <Container fluid className="mt-3">
-      {schoolData && (
-        <Row>
-          <Col md={4}>
-            <Card bg="light" className="rounded p-3 mb-3">
-              <h5 className="text-center mb-4">School Details</h5>
-              <div className="text-center mb-4">
-                <p><strong>Branch Name:</strong> {schoolData.school.branchName}</p>
-                <p><strong>Address:</strong> {schoolData.school.address}</p>
-                <p><strong>City:</strong> {schoolData.school.city}</p>
-                <p><strong>Number of Gates:</strong> {schoolData.school.numberOfGates}</p>
-                <p><strong>Status:</strong> {schoolData.school.status}</p>
-                <p><strong>Email:</strong> {schoolData.school.email}</p>
-              </div>
-            </Card>
-          </Col>
-          <Col md={6}>
-            <Row>
-              <Col>
-                <h2 className="mb-4 text-center">School Timings</h2>
-                <Card bg="light" className="rounded p-3 mb-3">
-                  <ul className="list-unstyled text-center">
-                    {schoolData.school.timings && schoolData.school.timings.map((timing, index) => (
-                      <li key={index} className="mb-2">
-                        <strong>{timing.day}:</strong> {formatLocalTime(timing.openTime)} - {formatLocalTime(timing.closeTime)}
-                      </li>
-                    ))}
-                  </ul>
-                </Card>
-                {!showUpdateForm && (
-                  <Button variant="primary" onClick={() => setShowUpdateForm(true)}>
-                    Add School Timings
-                  </Button>
-                )}
-              </Col>
-            </Row>
+    <div className="front-page" style={{ backgroundImage: `url(${logo})` }}>
+      <Container fluid className="mt-3">
+        {schoolData && (
+          <Row>
+<Col md={3}>
+  <Card bg="light" className="rounded p-3 mb-3" style={{ width: '350px' }}>
+    <div className="d-flex align-items-center mb-4">
+      <BiBuilding size={50} color="#007bff" className="mr-2" />
+      <h4 className="mb-0">School Details</h4>
+    </div>
+    <div style={{ overflowX: 'auto' }}> {/* Add this div with overflow style */}
+      <table className="table">
+        <tbody>
+          <tr>
+            <td><strong>Branch Name:</strong></td>
+            <td>{schoolData.school.branchName}</td>
+          </tr>
+          <tr>
+            <td><strong>Address:</strong></td>
+            <td>{schoolData.school.address}</td>
+          </tr>
+          <tr>
+            <td><strong>City:</strong></td>
+            <td>{schoolData.school.city}</td>
+          </tr>
+          <tr>
+            <td><strong>Number of Gates:</strong></td>
+            <td>{schoolData.school.numberOfGates}</td>
+          </tr>
+          <tr>
+            <td><strong>Status:</strong></td>
+            <td>{schoolData.school.status}</td>
+          </tr>
+          <tr>
+            <td><strong>Email:</strong></td>
+            <td>{schoolData.school.email}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </Card>
+</Col>
 
-            {showUpdateForm && (
-              <Row>
-                <Col md={{ span: 6, offset: 3 }}>
-                  <Card bg="light" className="rounded p-3 mb-3">
-                    <h5 className="text-center mb-4">Update Timings</h5>
-                    <Form onSubmit={handleTimingUpdate}>
-                      <Form.Group controlId="formDay">
-                        <Form.Control type="text" placeholder="Enter day" value={day} onChange={(e) => setDay(e.target.value)} required />
-                      </Form.Group>
-                      <Form.Group controlId="formOpenTime">
-                        <Form.Control type="time" value={openTime} onChange={(e) => setOpenTime(e.target.value)} required />
-                      </Form.Group>
-                      <Form.Group controlId="formCloseTime">
-                        <Form.Control type="time" value={closeTime} onChange={(e) => setCloseTime(e.target.value)} required />
-                      </Form.Group>
-                      <Button variant="primary" type="submit">Update</Button>
-                      <Button variant="secondary" onClick={() => setShowUpdateForm(false)}>Close</Button>
-                    </Form>
-                  </Card>
-                </Col>
-              </Row>
-            )}
-          </Col>
-          <Col md={1} className="text-center">
-            <StudentCountBox totalStudents={totalStudents} />
-          </Col>
-        </Row>
-      )}
-    </Container>
+
+            <Col md={6}>
+  <Card bg="light" className="rounded p-3 mb-3">
+    <div className="d-flex align-items-center mb-4">
+      <BiSolidWatch size={50} color="#007bff" className="mr-2" />
+      <h4 className="mb-0">School Timings</h4>
+    </div>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Day</th>
+          <th>Arrival</th>
+          <th>Departure</th>
+        </tr>
+      </thead>
+      <tbody>
+        {schoolData.school.timings && schoolData.school.timings.map((timing, index) => (
+          <tr key={index}>
+            <td>{timing.day}</td>
+            <td>{formatLocalTime(timing.openTime)}</td>
+            <td>{formatLocalTime(timing.closeTime)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </Card>
+  {!showUpdateForm && (
+    <Button variant="primary" onClick={() => setShowUpdateForm(true)}>
+      Add School Timings
+    </Button>
+  )}
+  {showUpdateForm && (
+    <Card bg="light" className="rounded p-3 mb-3">
+      <h5 className="text-center mb-4">Update Timings</h5>
+      <Form onSubmit={handleTimingUpdate}>
+        <Form.Group controlId="formDay">
+          <div className="position-relative">
+            <Form.Control as="select" value={day} onChange={(e) => setDay(e.target.value)} required>
+              <option value="" disabled>Select a day</option>
+              <option value="Monday">Monday</option>
+              <option value="Tuesday">Tuesday</option>
+              <option value="Wednesday">Wednesday</option>
+              <option value="Thursday">Thursday</option>
+              <option value="Friday">Friday</option>
+              <option value="Saturday">Saturday</option>
+              <option value="Sunday">Sunday</option>
+            </Form.Control>
+            <BiChevronDown className="position-absolute top-50 end-0 translate-middle-y" style={{ fontSize: '1.5rem', color: '#007bff', marginRight: '5px' }} />
+          </div>
+        </Form.Group>
+        <Form.Group controlId="formOpenTime">
+          <Form.Control type="time" value={openTime} onChange={(e) => setOpenTime(e.target.value)} required />
+        </Form.Group>
+        <Form.Group controlId="formCloseTime">
+          <Form.Control type="time" value={closeTime} onChange={(e) => setCloseTime(e.target.value)} required />
+        </Form.Group>
+        <Button variant="primary" type="submit">Update</Button>
+        <Button variant="secondary" onClick={() => setShowUpdateForm(false)}>Close</Button>
+      </Form>
+    </Card>
+  )}
+  </Col>
+ <Col md={3} className="d-flex flex-column align-items-end">
+    {/* Column for Logo, Student Count, and Driver Count */}
+    <img src={logo} alt="Logo" style={{ width: '150px', marginRight:"100px" }} />
+    <StudentCountBox totalStudents={totalStudents} />
+    <Card bg="light" className="rounded p-2 mt-3" style={{ width: '200px', border: '1px solid black', boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)', marginRight:"60px" }}>
+      <div className="d-flex align-items-center mb-2">
+        <BiCar size={30} color="#007bff" className="mr-2" />
+        <h5 className="mb-0">Drivers</h5>
+      </div>
+      <div className="text-center">
+        <p><strong>Total:</strong> {totalDrivers}</p>
+      </div>
+    </Card>
+  </Col>
+  
+</Row>
+        )}
+      </Container>
+    </div>
   );
 }
 
